07681f3151cf0858deef0d4a33887278f229138d
Nevin100 <opopopopop100000@gmail.com>
2026-01-06 22:16:34 +0530

    Completed and Patched Vulnerabilities

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -3,15 +3,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB01g5j59-qx0UFH3FLtB48tikomB9SQtI",
-  authDomain: "spark-1c644.firebaseapp.com",
-  projectId: "spark-1c644",
-  storageBucket: "spark-1c644.appspot.com",
-  messagingSenderId: "228171804713",
-  appId: "1:228171804713:web:8e4da059781a026a38ec38",
-  measurementId: "G-QRMVHZSSEW"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase services
